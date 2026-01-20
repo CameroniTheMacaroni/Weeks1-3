de9cbf7627c8bfa5dde8bf6ecf1b14150797aefc
Cameron Sensicle <sensicle@shernet.sheridancollege.ca>
2026-01-20 10:56:02 -0500

    added arraylists to change the sprites

--- Assets/Scripts/SpriteChanger.cs
+++ Assets/Scripts/SpriteChanger.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,6 +6,8 @@ public class SpriteChanger : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public Color col;
+    public List<Sprite> barrels;
+    public int randomNumber;
     void Start()
     {
         
@@ -13,10 +16,12 @@ public class SpriteChanger : MonoBehaviour
     void Update()
     {
 
-        //if (Keyboard.current.anyKey.wasPressedThisFrame)
-        //{
-        //    PickARandomColor();
-        //}
+        if (Keyboard.current.anyKey.wasPressedThisFrame)
+        {
+            //PickARandomColor();
+            PickARandomSprite();
+
+        }
 
         //get mouse position
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
@@ -24,6 +29,7 @@ public class SpriteChanger : MonoBehaviour
         if (spriteRenderer.bounds.Contains(mousePos))
         {
             //y: set the color to col variable
+            //spriteRenderer.sprite = barrels[3];
             spriteRenderer.color = col;
         }
         else
@@ -31,11 +37,24 @@ public class SpriteChanger : MonoBehaviour
             //n: set colour to white
             spriteRenderer.color = Color.white;
         }
+        if (Mouse.current.leftButton.wasPressedThisFrame && barrels.Count > 0)
+        {
+            randomNumber = Random.Range(0, barrels.Count); //choose a random number
+            barrels.RemoveAt(randomNumber);
+        }
     }
 
     void PickARandomColor()
     {
         //spriteRenderer.color = Random.ColorHSV();
         spriteRenderer.color = col;
+    }
+    void PickARandomSprite()
+    {
+        //spriteRenderer.sprite = mySprite;
+        randomNumber = Random.Range(0, barrels.Count); //choose a random number
+        //use that number to choose a sprite
+        //assign that sprite to the sprite renderer
+        spriteRenderer.sprite = barrels[randomNumber];
     }
 }
